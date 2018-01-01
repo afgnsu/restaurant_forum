541c07e3a91c7e87af9aa4eeba9f395844317455
francishuang <francis3925@gmail.com>
2018-01-01 18:22:41 +0800

    creat comment model and setup relation between restaurant, user and comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :comments
+
   def admin?
     self.role == "admin"
   end
+
 end
