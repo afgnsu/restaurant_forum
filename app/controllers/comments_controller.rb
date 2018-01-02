605a0323e9e1a6257e78ea5508507bb2f40b5f5c
francishuang <francis3925@gmail.com>
2018-01-02 23:30:50 +0800

    enable admin user to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,19 @@ class CommentsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = @restaurant.comments.build(comment_params)
-    @comment = current_user
+    @comment.user = current_user
     @comment.save!
     redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = Comment.find(params[:id])
+
+    if current_user.admin?
+      @comment.destroy
+      redirect_to restaurant_path(@restaurant)
+    end
   end
 
   private
