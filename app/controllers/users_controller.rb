26e187765f5efeae9e2b97165ef370123bed2382
francishuang <francis3925@gmail.com>
2018-01-06 23:19:20 +0800

    setup users controller and modify user edit & show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,30 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(:email, :name, :intro, :avatar)
+  end
 
 end
