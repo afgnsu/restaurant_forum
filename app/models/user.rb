1d6eb05663dfd2f374b766fe2a043c53cb4545bb
francishuang <francis3925@gmail.com>
2018-01-19 23:16:27 +0800

    create model like and setup routes for like and unlike

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
+
   validates_presence_of :name
 
   mount_uploader :avatar, AvatarUploader
